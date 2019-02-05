6e23df5b9fee7c6cf20af168574fa59b2530cdd7
Jantz <jantzillac.93@gmail.com>
2019-02-04 19:20:41 -0600

    Add functionality to "init()"

--- autohinttextview/src/main/java/com/apps/creativesource/autohinttextview/AutoHintTextView.java
+++ autohinttextview/src/main/java/com/apps/creativesource/autohinttextview/AutoHintTextView.java
@@ -2,11 +2,30 @@ package com.apps.creativesource.autohinttextview;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Build;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+
 public class AutoHintTextView extends LinearLayout {
+
+    Paint paint;
+    Rect rect;
+    int squareColor, entryEnd;
+    private EditText hintEditText, entryEditText;
+    private ArrayList<String> suggestions;
 
     public AutoHintTextView(Context context) {
         super(context);
@@ -30,6 +49,49 @@ public class AutoHintTextView extends LinearLayout {
     }
 
     private void init(final Context context, AttributeSet set) {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        rect = new Rect();
+        suggestions = new ArrayList<>();
+
+        LayoutInflater.from(context).inflate(R.layout.auto_hint_text_view, this, true);
+
+        hintEditText = findViewById(R.id.et_hint);
+        entryEditText = findViewById(R.id.et_entry);
+
+        entryEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                entryEnd = count;
+
+                String resultString = searchStrings(String.valueOf(s));
+
+                Spannable wordToSpan = new SpannableString(resultString);
+
+                if(wordToSpan.length() > 0) {
+                    wordToSpan.setSpan(new ForegroundColorSpan(Color.GRAY), entryEnd, wordToSpan.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    hintEditText.setText(wordToSpan);
+                } if(count == 0 || wordToSpan.length() == 0)
+                    hintEditText.setText("");
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        if(set == null)
+            return;
+
+        TypedArray ta = context.obtainStyledAttributes(set, R.styleable.AutoHintTextView);
+        squareColor = ta.getColor(R.styleable.AutoHintTextView_square_color, Color.WHITE);
+        paint.setColor(squareColor);
+        ta.recycle();
 
     }
 }
