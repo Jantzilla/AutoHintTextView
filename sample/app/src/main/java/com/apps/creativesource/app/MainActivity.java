05bf265084c873b7968a84bf9d6cddf178a53443
Jantz <jantzillac.93@gmail.com>
2019-02-04 21:52:25 -0600

    Reference new "AutoHintTextView" & set example "suggestions"

--- sample/app/src/main/java/com/apps/creativesource/app/MainActivity.java
+++ sample/app/src/main/java/com/apps/creativesource/app/MainActivity.java
@@ -3,11 +3,32 @@ package com.apps.creativesource.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.apps.creativesource.autohinttextview.AutoHintTextView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    AutoHintTextView autoHintTextView;
+    ArrayList<String> suggestions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        autoHintTextView = findViewById(R.id.tv_auto_hint);
+
+        suggestions = new ArrayList<>();
+
+        suggestions.add("Blue");
+        suggestions.add("Green");
+        suggestions.add("Pink");
+        suggestions.add("Red");
+        suggestions.add("Orange");
+        suggestions.add("Black");
+        suggestions.add("White");
+        suggestions.add("Purple");
+        suggestions.add("Yellow");
+
+        autoHintTextView.setSuggestions(suggestions);
     }
 }
