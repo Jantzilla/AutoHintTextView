2e73dba7b9aefb1255d9994cf335c58e81b2d57a
Jantz <jantzillac.93@gmail.com>
2019-02-12 01:58:28 -0600

    Change "suggestions" ArrayList & set "addHintChangedListener()"

--- sample/app/src/main/java/com/creativesourceapps/android/app/MainActivity.java
+++ sample/app/src/main/java/com/creativesourceapps/android/app/MainActivity.java
@@ -1,14 +1,18 @@
 package com.creativesourceapps.android.app;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 
-import com.creativesourceapps.android.app.R;
 import com.creativesourceapps.android.autohinttextview.AutoHintTextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ConstraintLayout root;
     AutoHintTextView autoHintTextView;
     ArrayList<String> suggestions;
 
@@ -17,19 +21,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         autoHintTextView = findViewById(R.id.tv_auto_hint);
+        root = findViewById(R.id.cl_root);
 
         suggestions = new ArrayList<>();
 
         suggestions.add("Blue");
         suggestions.add("Green");
-        suggestions.add("Pink");
+        suggestions.add("Magenta");
         suggestions.add("Red");
-        suggestions.add("Orange");
-        suggestions.add("Black");
-        suggestions.add("White");
-        suggestions.add("Purple");
+        suggestions.add("Gray");
         suggestions.add("Yellow");
 
         autoHintTextView.setSuggestions(suggestions);
+
+        autoHintTextView.addHintChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                switch (String.valueOf(s)) {
+                    case "Blue":
+                        root.setBackgroundColor(Color.BLUE);
+                        break;
+                    case "Green":
+                        root.setBackgroundColor(Color.GREEN);
+                        break;
+                    case "Red":
+                        root.setBackgroundColor(Color.RED);
+                        break;
+                    case "Cyan":
+                        root.setBackgroundColor(Color.CYAN);
+                        break;
+                    case "Gray":
+                        root.setBackgroundColor(Color.GRAY);
+                        break;
+                    case "Yellow":
+                        root.setBackgroundColor(Color.YELLOW);
+                        break;
+                    case "Magenta":
+                        root.setBackgroundColor(Color.MAGENTA);
+                        break;
+                    default:
+                        root.setBackgroundColor(Color.WHITE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
